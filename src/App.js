e681c66f11a825b9accfab3b92814a36344ae570
Julien-FRA <julien.francois@hetic.net>
2023-03-21 17:10:11 +0100

    fix bug test

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          TicTacToe app
+          Learn react
         </a>
       </header>
     </div>
